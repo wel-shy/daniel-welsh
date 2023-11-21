b459ef7fedf3e0adc7eb7f6a32974c6b3783cedb
Daniel Welsh <danjwelsh24@gmail.com>
2023-11-21 19:21:35 +0100

    styling work experience details

--- src/components/Timeline/Item.tsx
+++ src/components/Timeline/Item.tsx
@@ -5,7 +5,14 @@ import styled from "styled-components";
 const Wrapper = styled.div<{ isSelected: boolean; percent?: number }>`
   background-color: ${({ isSelected }) => (isSelected ? "red" : "gray")};
   flex-basis: ${({ percent }) => percent}%;
+  transition: 0.5s ease all;
   width: 100%;
+
+  &:hover {
+    background-color: blue;
+    cursor: pointer;
+    font-weight: bold;
+  }
 `;
 
 interface Props {
--- src/components/Timeline/RoleDetails.tsx
+++ src/components/Timeline/RoleDetails.tsx
@@ -1,17 +1,53 @@
 import React from "react";
 import { Role } from "./types";
+import styled from "styled-components";
+
+const Wrapper = styled.div`
+  & .role-header {
+    align-items: center;
+    display: flex;
+    margin-bottom: 0em;
+
+    h3 {
+      flex: 1;
+      font-size: 2em;
+      margin-bottom: 0em;
+    }
+
+    p {
+      margin-bottom: 0em;
+    }
+  }
+
+  h4 {
+    font-size: 1.5em;
+    margin-bottom: 0.25em;
+  }
+
+  & .location {
+    margin: 0;
+  }
+
+  // TODO: Make response to screen width.
+  .description {
+    max-width: 50vw;
+  }
+`;
 
 const RoleDetails = ({ role }: { role: Role }) => {
   return (
-    <div>
-      <h3>{role.role}</h3>
-      <h4>{role.company}</h4>
-      <p>
-        {role.from.toDateString()} - {role.to.toDateString()}
-      </p>
-      <p>{role.location}</p>
-      <p>{role.description}</p>
-    </div>
+    <Wrapper>
+      <div className="role-header">
+        <h3>{role.company}</h3>
+        <p>
+          {role.from.toDateString()} - {role.to.toDateString()}
+        </p>
+      </div>
+      <h4>{role.role}</h4>
+
+      <p className="location">{role.location}</p>
+      <p className="description">{role.description}</p>
+    </Wrapper>
   );
 };
 
