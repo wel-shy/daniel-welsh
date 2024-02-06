6b1184cd2ddda56236881f666e0da5e3aef74fe4
Daniel Welsh <danjwelsh24@gmail.com>
2024-02-06 21:06:21 +0100

    extracting global style

---
+++ src/components/GlobalStyle/GlobalStyle.ts
@@ -0,0 +1,31 @@
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: #1f1f1f;
+    color: #cfcfcf;
+    font-family: -apple-system, Roboto, sans-serif, serif;
+    font-size: 14px;
+    padding: 2em;
+    min-height: 100vh;
+  }
+
+  p {
+    margin-bottom: 2em;
+  }
+
+  code {
+    background-color: "#FFF4DB";
+    border-radius: 4px;
+    color: "#8A6534";
+    font-size: "1.25rem";
+    padding: 4px;
+  }
+
+  ul {
+    margin-bottom: 96;
+    padding-left: 0;
+  }
+`;
+
+export default GlobalStyle;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,37 +1,10 @@
 import * as React from "react";
 import type { HeadFC, PageProps } from "gatsby";
 import Header from "../components/Header/Header";
-import styled, { createGlobalStyle } from "styled-components";
+import styled from "styled-components";
 import SkillsList from "../components/SkillsList/SkillsList";
 import Timeline from "../components/Timeline/Timeline";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    background-color: #1f1f1f;
-    color: #cfcfcf;
-    font-family: -apple-system, Roboto, sans-serif, serif;
-    font-size: 14px;
-    padding: 2em;
-    min-height: 100vh;
-  }
-
-  p {
-    margin-bottom: 2em;
-  }
-
-  code {
-    background-color: "#FFF4DB";
-    border-radius: 4px;
-    color: "#8A6534";
-    font-size: "1.25rem";
-    padding: 4px;
-  }
-
-  ul {
-    margin-bottom: 96;
-    padding-left: 0;
-  }
-`;
+import GlobalStyle from "../components/GlobalStyle/GlobalStyle";
 
 const ContentWrapper = styled.div`
   border: solid white 1px;
